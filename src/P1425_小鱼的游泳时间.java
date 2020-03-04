35e016a340fc4d1de3a24c1606e506870edc3a93
yuanchen <yuanchenshi@gmail.com>
2020-03-04 12:20:08 -0600

    小鱼的游泳时间

---
+++ src/P1425_小鱼的游泳时间.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P1425_小鱼的游泳时间 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int d = sc.nextInt();
+    int minutes = c * 60 + d - a * 60 - b;
+    int hour = minutes / 60;
+    int min = minutes % 60;
+    System.out.println(hour + " " + min);
+  }
+}
