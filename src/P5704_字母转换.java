73062c60caa190f5eaec2a27b1f1b42dac92544a
yuanchen <yuanchenshi@gmail.com>
2020-03-08 09:04:53 -0500

    字母转换

---
+++ src/P5704_字母转换.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P5704_字母转换 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String c = sc.next();
+		System.out.println(c.toUpperCase());
+	}
+}
