249e0df851704b51c855e80357fd3f789f553ed5
yuanchen <yuanchenshi@gmail.com>
2020-03-10 09:51:12 -0500

    车厢重组

---
+++ src/P1116_车厢重组.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class P1116_车厢重组 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] > arr[j]) {
+					swap(arr, i, j);
+					cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+
+
+	private static void swap(int[] arr, int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+
+}
